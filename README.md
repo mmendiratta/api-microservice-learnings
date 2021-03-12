941c2a8cf8da1c518304bdf3e72acb735e359e36
ManavMendiratta <Manav.Mendiratta@target.com>
2021-03-11 20:07:22 -0600

    added env file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# [Introduction to the Basic Node and Express Challenges](https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/)
+# api-microservice-learnings
+Here I am learning more about the principles of apis and microservices. I am following a tutorial through FreeCodeCamp, and will work towards getting their certification. 
--- myApp.js
+++ myApp.js
@@ -3,4 +3,21 @@ var app = express();
 
 console.log("Hello world!");
 
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+    console.log("req ", req)
+    console.log("res ", res)
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/json", (res) => {
+    var message = {"message": "Hello json"};
+   
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message.message.toUpperCase();
+    }
+    res.json(message);
+})
+    
+
  module.exports = app;
